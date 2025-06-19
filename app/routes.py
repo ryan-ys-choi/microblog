ef1806982cd86e83a2160bf1ce2f445f7c912f54
ryan-ys-choi <104918388+ryan-ys-choi@users.noreply.github.com>
2025-06-19 13:39:21 -0700

    added log out function

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,11 @@ from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
+from flaks_login import current_user, login_user
+import sqlalchemy as sa
+from app import dh
+from app.models import User
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -20,10 +25,17 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     # validate_on_submit() is part of Flask-WTF
     if form.validate_on_submit():
-        flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
             
