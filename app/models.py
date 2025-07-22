f4ddc2cb8e4a47e009f3819d5c24627068d0aa9e
ryan-ys-choi <104918388+ryan-ys-choi@users.noreply.github.com>
2025-07-22 11:05:42 -0700

    avarta added

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from app import login
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -22,6 +23,10 @@ class User(UserMixin, db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 @login.user_loader
 def load_user(id):
