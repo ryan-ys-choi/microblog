01d3a1e0b04da6a7352f046dba1819e28e7b0fdb
ryan-ys-choi <104918388+ryan-ys-choi@users.noreply.github.com>
2025-07-27 14:21:16 -0700

    Obtaining the Posts from Followed Users

--- app/models.py
+++ app/models.py
@@ -45,7 +45,40 @@ class User(UserMixin, db.Model):
         secondary=followers, primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         back_populates='following')
-                                                            
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.following.add(user)
+    
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.following.remove(user)
+    
+    def is_following(self, user):
+        query = self.following.select().where(User.id == user.id)
+        return db.session.scalar(query) is not None
+    
+    def followers_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.followers.select().subquery())
+        return db.session.scalar(query)
+    
+    def following_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.following.select().subquery())
+        return db.session.scalar(query)
+    
+    def following_posts(self):
+        Author = so.aliased(User)
+        Follower = so.aliased(User)
+        return (
+            sa.select(Post)
+            .join(Post.author.of_type(Author))
+            .join(Author.followers.of_type(Follower))
+            .where(Follower.id == self.id)
+            .order_by(Post.timestamp.desc())
+        )
+                                                                    
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
