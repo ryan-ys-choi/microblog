b72e5b20bac51767fc0079cd23e58c1d21ec427f
ryan-ys-choi <104918388+ryan-ys-choi@users.noreply.github.com>
2025-08-01 08:23:00 -0700

    added view func for password reset

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,8 @@ from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app.models import User, Post
 from datetime import datetime, timezone
-from app.forms import EmptyForm, PostForm
+from app.forms import EmptyForm, PostForm, ResetPasswordRequestForm
+from app.email import send_password_reset_email
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -151,4 +152,18 @@ def explore():
     prev_url = url_for('explore', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
-        
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.email == form.email.data))
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to rest your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html', title='Reset Password', form=form)
+        )
